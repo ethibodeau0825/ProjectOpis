39c856d3c96d3a1be2df1e38c629474e77fb5e75
imajdoch <98667979+imajdoch@users.noreply.github.com>
2022-11-20 14:07:51 -0500

    Update .acquire.sh

--- Control_Room/.acquire.sh
+++ Control_Room/.acquire.sh
@@ -10,32 +10,32 @@ then
 		"doc_1")
 			echo "doc_1" >> inventory
 			echo "You acquired Document 1!"
-			doc_count++
+			$doc_count++
 		;;
 		"doc_2")
 			echo "doc_2" >> inventory
 			echo "You acquired Document 2!"
-			doc_count++
+			$doc_count++
 		;;
 		"doc_3")
 			echo "doc_3" >> inventory
 			echo "You acquired Document 3!"
-			doc_count++
+			$doc_count++
 		;;
 		"doc_4")
 			echo "doc_4" >> inventory
 			echo "You acquired Document 4!"
-			doc_count++
+			$doc_count++
 		;;
 		"doc_5")
 			echo "doc_5" >> inventory
 			echo "You acquired Document 5!"
-			doc_count++
+			$doc_count++
 		;;
 		"doc_6")
 			echo "doc_6" >> inventory
 			echo "You acquired Document 6!"
-			doc_count++
+			$doc_count++
 		;;
 		"")
 			echo "You did not enter any item to your inventory!"
