f0c0e9a145bf5341fd1381ac65e5fdc461fd384c
faithpyang <114090353+faithpyang@users.noreply.github.com>
2022-11-21 21:59:51 -0500

    Create .acquire.sh

---
+++ Control_Room/Medical_Room/.acquire.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Please enter item: "
+read item
+echo
+user=$PWD
+grep -q $item $user/inventory
+if [ $? -eq 0 ]; then
+	echo "$item is already in your inventory!"
+	echo
+	exit 0
+fi
+
+echo "$item" >> inventory
+echo "You acquired $item!"
+echo
+
+rsync -av --ignore-existing $user/inventory $user/Lab_Room/inventory
