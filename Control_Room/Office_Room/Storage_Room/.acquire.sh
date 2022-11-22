0f1f2be13431bca1eb95a78ff945e67310dff703
faithpyang <114090353+faithpyang@users.noreply.github.com>
2022-11-22 11:11:59 -0500

    Create .acquire.sh

---
+++ Control_Room/Office_Room/Storage_Room/.acquire.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Please enter item: "
+read item
+echo
+user=$PWD
+grep -q $item $user/inventory
+if [ $? -eq 0 ]; then
+	echo "$item is already in your inventory!"
+	echo
+	exit 0
+fi
+
+echo "$item" >> inventory
+echo "You acquired $item!"
+echo
+
+rsync -av $user/inventory ~/ProjectOpis/Control_Room/inventory
