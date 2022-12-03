50af7bac7cbd2118f4e74b061d41c95054ef5677
faithpyang <114090353+faithpyang@users.noreply.github.com>
2022-12-03 15:54:06 -0500

    Update .acquire.sh

--- Control_Room/Office_Room/.acquire.sh
+++ Control_Room/Office_Room/.acquire.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 echo "Please enter item: "
 read item
-echo
+if [[ $item == "tool_box" || $item == "doc_4" || $item == "Purple_Key" ]]; then
+	echo "$item" >> inventory
+	echo "You acquired $item!"
+	echo
+else 
+	echo "This item does not seem to fit in your inventory..."
+	echo
+fi
 user=$PWD
 grep -q $item $user/inventory
 if [ $? -eq 0 ]; then
@@ -10,9 +17,6 @@ if [ $? -eq 0 ]; then
 	exit 0
 fi
 
-echo "$item" >> inventory
-echo "You acquired $item!"
-echo
 
 rsync -avq $user/inventory ~/ProjectOpis/Control_Room/inventory
 rsync -avq $user/inventory $user/Storage_Room/inventory
