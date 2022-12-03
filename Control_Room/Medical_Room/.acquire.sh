355e1f624ed9ff31c39c94bac34777e7f3fdcaaf
Faith Yang <fyang1576@floridapoly.edu>
2022-12-03 15:45:58 -0500

    acquire fixed in medical

--- Control_Room/Medical_Room/.acquire.sh
+++ Control_Room/Medical_Room/.acquire.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 echo "Please enter item: "
 read item
-echo
+if [[ $item == "doc_2" || $item == "note" || $item == "Scalpel" || $item == "Red_Key" ]]; then
+	echo "$item" >> inventory
+	echo "You acquired $item!"
+	echo
+fi
+
 user=$PWD
 grep -q $item $user/inventory
 if [ $? -eq 0 ]; then
