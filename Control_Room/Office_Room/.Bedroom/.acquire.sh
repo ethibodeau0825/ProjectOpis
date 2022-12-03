f07810629ce91bec594a22e83e3bdf9e204b6f28
faithpyang <114090353+faithpyang@users.noreply.github.com>
2022-12-03 17:02:43 -0500

    Update .acquire.sh

--- Control_Room/Office_Room/.Bedroom/.acquire.sh
+++ Control_Room/Office_Room/.Bedroom/.acquire.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 echo "Please enter item: "
 read item
+user=$PWD
+grep -q $item $user/inventory
+if [ $? -eq 0 ]; then
+	echo "$item is already in your inventory!"
+	echo
+	exit 0
+fi
 if [[ $item == "doc_6" || $item == "Pink_Key" || $item == "Green_Key" ]]; then
 	echo "$item" >> inventory
 	echo "You acquired $item!"
@@ -8,13 +15,6 @@ if [[ $item == "doc_6" || $item == "Pink_Key" || $item == "Green_Key" ]]; then
 else
 	echo "This item doesn't seem to fit your inventory..."
 	echo
-fi
-user=$PWD
-grep -q $item $user/inventory
-if [ $? -eq 0 ]; then
-	echo "$item is already in your inventory!"
-	echo
-	exit 0
 fi
 
 rsync -avq $user/inventory ~/ProjectOpis/Control_Room/inventory
