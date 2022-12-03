e542ecb312df4de6695ed7c5f0de78247f93a3e5
faithpyang <114090353+faithpyang@users.noreply.github.com>
2022-12-03 18:58:28 -0500

    Update .acquire.sh

--- Control_Room/Medical_Room/.acquire.sh
+++ Control_Room/Medical_Room/.acquire.sh
@@ -14,11 +14,9 @@ if [[ $item == "doc_2" || $item == "note" || $item == "Scalpel" || $item == "Red
 	echo "$item" >> inventory
 	echo "You acquired $item!"
 	echo
-	exit 0
 else 
 	echo "This item does not seem to fit in your inventory..."
 	echo
-	exit 0
 fi
 
 cp -aRp ~/ProjectOpis/Control_Room/Medical_Room/inventory ~/ProjectOpis/Control_Room/Medical_Room/Lab_Room/inventory
