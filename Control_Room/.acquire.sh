4afef8ada29fd2d1a9f0f004283df6324574648c
faithpyang <114090353+faithpyang@users.noreply.github.com>
2022-12-03 16:12:53 -0500

    Update .acquire.sh

--- Control_Room/.acquire.sh
+++ Control_Room/.acquire.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 echo "Please enter item: "
 read item
-if [[ $item == "doc_1" ]]; then
-	echo "$item" >> inventory
-	echo "You acquired $item!"
-	echo
-else 
-	echo "This item does not seem to fit in your inventory..."
-	echo
-fi
 user=$PWD
 grep -q $item $user/inventory
 if [ $? -eq 0 ]; then
 	echo "$item is already in your inventory!"
 	echo
 	exit 0
+fi
+
+if [[ $item == "doc_1" ]]; then
+	echo "$item" >> inventory
+	echo "You acquired $item!"
+	echo
+#else 
+#	echo "This item does not seem to fit in your inventory..."
+#	echo
 fi
 
 rsync -avq $user/inventory $user/Medical_Room/inventory
